5a4765ed5728bc9c175292c1046821a2c5f5f9ba
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-10-17 10:48:48 +0100

    index styles

--- resources/views/clothes/index.blade.php
+++ resources/views/clothes/index.blade.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VirtualClothing</title>
 </head>
 <body>
-    <h1>Clothes</h1>
-    <ul>
-        @foreach ($clothes as $clothes)
-            <li>{{$clothes->title}} {{$clothes->brand}} {{$clothes->description}} {{$clothes->price}}</li>
-            <a href="{{ route('clothes.edit', $clothes) }}">Edit</a>
-            <form action="{{ route('clothes.destroy', $clothes->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>  
-        @endforeach
-    </ul>
+    <header>
+        <h1 class="title">Clothes</h1>
+    </header>
+    <main class="main-wrapper">
+        <a class="home-btn" href="{{url('/')}}">< Home</a>
+        <ul>
+            @foreach ($clothes as $clothes)
+                <h2 class="item-number">{{$clothes->id}}</h2>
+                <li class="item-title">{{$clothes->title}}</li><li class="item-brand">{{$clothes->brand}}</li> <li>{{$clothes->description}}</li> <li>{{$clothes->price}}</li>
+                <div class="btns">
+                    <a class="edit-btn" href="{{ route('clothes.edit', $clothes) }}">Edit</a>
+                    <form action="{{ route('clothes.destroy', $clothes->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="delete-btn" type="submit">Delete</button>
+                    </form>
+                </div>
+                <hr>
+            @endforeach
+        </ul>
+    </main>
 </body>
 </html>
